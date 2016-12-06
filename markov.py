50b5e5a187078cd57e1cccc5ed23b116c3e3fcc1
joemCruz <joe_cruz5555@yahoo.com>
2016-12-06 12:57:16 -0700

    added print for analysis type

--- markov.py
+++ markov.py
@@ -20,8 +20,9 @@ class Markov(GeoAlgorithm):
     CONTIGUITY = 'CONTIGUITY'
     MARKOV_T = 'MARKOV_T'
     TRANSITIONS = 'TRANSITIONS'
+    ANALYSIS_T = 'ANALYSIS_T'
     MAP_OUTPUT = 'MAP_OUTPUT'
-
+    
     def defineCharacteristics(self):
         self.name = "Markov"
         self.group = 'Spatial Dynamics'
@@ -33,17 +34,18 @@ class Markov(GeoAlgorithm):
 
         self.addParameter(ParameterVector(self.INPUT,
             self.tr('Input Layer'), [ParameterVector.VECTOR_TYPE_POLYGON]))
-        #Added Markov parameter
+        # Added Markov parameter
         self.addParameter(ParameterSelection(self.MARKOV_T,
             self.tr('Markov Type'), ['Classic', 'Spatial', 'LISA']))
-        #Beginning and end field parameters to read
+        self.addParameter(ParameterSelection(self.ANALYSIS_T,
+            self.tr('Analysis Type'), ['End Point', 'Majority Moves']))
+        # Beginning and end field parameters to read
         self.addParameter(ParameterTableField(self.FIELD_START,
             self.tr('Starting Field'), self.INPUT))
         self.addParameter(ParameterTableField(self.FIELD_END,
             self.tr('Ending Field'), self.INPUT))
         self.addParameter(ParameterSelection(self.FIELD_JUMPS,
             self.tr('Fields to Jump'), ['1','2','3','4','5']))
-        
         self.addParameter(ParameterSelection(self.CONTIGUITY,
             self.tr('Contiguity'), ['Queen','Rook']))
 
@@ -64,21 +66,27 @@ class Markov(GeoAlgorithm):
         fields = provider.fields()
         # Can append new data to the fields variable. This will
         # be used later to enter newly calculated data into the
-        # given, appended field.
-        # using fields.append(...) i.e:
-        fields.append(QgsField('MARKOV_RES', QVariant.Int))
+        # given, appended field. Using fields.append(...) i.e:
 
+        # Adds the field MARKOV_RES to the end of the list of fields.
+        # This field will be used to add a new layer to the QGIS canvas.
+        fields.append(QgsField('MARKOV_RES', QVariant.Int))
         field_names = [field.name() for field in fields]
+        
         # Retrieve the index values for the fields
         # Use these as the starting and stopping points
-
         start_index = field_names.index(field_s)
         end_index = field_names.index(field_e)
-        print field_names[start_index], field_names[end_index]
+        print ("Field Range: {0} : {1}".format(field_names[start_index], field_names[end_index]))
+        print (self.getParameterValue(self.FIELD_JUMPS))
+        jumps = int(self.getParameterValue(self.FIELD_JUMPS)) + 1
+        print ("Fields Read:")
+        for i in range(start_index, end_index+1, jumps):
+            print (field_names[i])
 
-        jumps = self.getParameterValue(self.FIELD_JUMPS)
         contiguity = self.getParameterValue(self.CONTIGUITY)
         markov_type = self.getParameterValue(self.MARKOV_T)
+        analysis_type = self.getParameterValue(self.ANALYSIS_T)
 
         # String placeholder using the parameter MARKOV_T
         mt = ''
@@ -103,61 +111,90 @@ class Markov(GeoAlgorithm):
         header = f.header
         start_index = header.index(field_s)
         end_index = header.index(field_e)
-        variables = [header[i] for i in range(start_index, end_index+1)]
+        variables = [header[i] for i in range(start_index, end_index+1, jumps)]
         pci = np.array([f.by_col[v] for v in variables])
         q5 = np.array([pysal.Quantiles(y).yb for y in pci]).transpose()
         pci = pci.astype(float)
-        print pci.shape
-        print q5.shape
+        #print pci.shape
+        #print q5.shape
 
 
-        # Performin the Markov chain analysis
+        # Performing the Markov chain analysis
         if markov_type == 0: # Classic Markov
             m = pysal.Markov(q5)
+            print ("INFO: Classic Markov")
+            print ("INFO: Transitions:\n {0}".format(m.transitions))
+            print ("INFO: Steady State Matrix:\n {0}".format(m.steady_state))
+            print ("INFO: P-Matrix:\n {0}".format(m.p))
         elif markov_type == 1: # Spatial Markov
-            m = pysal.Spatial_Markov(pci, w, fixed = True, k = 5,
+            m = pysal.Spatial_Markov(pci.transpose(), w, fixed = True, k = 5,
                 variable_name = 'spatialMarkovVar')
+            print ("INFO: Spatial Markov")
+            print ("INFO: A-Spatial Measures")
+            print ("A-Spatial Transitions: \n {0}".format(m.transitions))
+            print ("A-Spatial Steady State Matrix: \n {0}".format(m.s))
+            print ("P-Matrix: \n {0}".format(m.p))
+            print ("INFO: Spatial Measures:")
+            print ("Spatial Transitions: \n {0}".format(m.T))
+            print ("Spatial Steady State Matrix: \n {0}".format(m.S))
+            print ("Spatial P-Matrix: \n {0}".format(m.P))
+            #print ("First Mean Passage Times: \n {0}".format(m.F))
         else: # LISA Markov
-            m = pysal.LISA_Markov(pci, w, 100)
-            
-        print m.transitions
+            m = pysal.LISA_Markov(pci.transpose(), w, 100)
+            print ("INFO: LISA Markov")
+            print ("INFO: Chi2 Statistics: {0}".format(m.chi_2))
+            print ("INFO: Transitions: \n {0}".format(m.transitions))
+            print ("INFO: Steady State Matrix:\n {0}".format(m.steady_state))
+            print ("INFO: P-Matrix:\n {0}".format(m.p))
 
-        # Endpoint data, for now, this is depicted in the final map
+#----------------------------------------------------------------------
+# Analysis Types
+
+        # Endpoint data
         # Takes the first value of a geometry and the last value then
         # finds the difference between the two.
-        i = 0
-        quants_ends_difs = []
-        for q in q5:
-            #print (q)
-            dif = q[-1] - q[0]
-            if dif > 0:
-                quants_ends_difs.append(2)
-            elif dif < 0:
-                quants_ends_difs.append(0)
-            else: # dif == 0
-                quants_ends_difs.append(1)
-        print quants_ends_difs
+        analysis_result = []
+        if analysis_type == 0:
+            print ("Analysis Type: Endpoints")
+            for q in q5:
+                #print (q)
+                dif = q[-1] - q[0]
+                if dif > 0:
+                    analysis_result.append(2)
+                elif dif < 0:
+                    analysis_result.append(0)
+                else: # dif == 0
+                    analysis_result.append(1)
+        # Majority Moves
+        # Finds the number of up, down or stationary moves of a geo-
+        # metry and shows the majority number of types of moves it
+        # makes.
+        elif analysis_type == 1:
+            print ("Analysis Type: Majority Moves")
+            for q in q5:
+                up = 0
+                stay = 0
+                down = 0
+                for j in range(len(q)-1):
+                    move = q[j+1] - q[j]
+                    if move > 0: #upwards moves
+                        up += 1
+                    elif move == 0: #stationary moves
+                        stay += 1
+                    elif move < 0: #downards moves
+                        down += 1
 
-        
-        # Handling Map Output
-        """
-        writer = self.getOutputFromName(self.MAP_OUTPUT).getVectorWriter(fields, provider.geometryType(), layer.crs())
-        outFeat = QgsFeature()
-        i = 0
-        for inFeat in processing.features(layer):
-            inGeom = inFeat.geometry()
-            outFeat.setGeometry(inGeom)
-            attribs = inFeat.attribute()
-            attribs.append(int(quants_ends_difs[i]))
-            outFeat.setAttributes(attribs)
-            writer.addFeature(outFeat)
-            i += 1
-
-        del writer
-
-            
-        out_layer = dataobjects.getObjectFromUri(self.getOutputValue(self.MAP_OUTPUT))
-        """
+                if max(up, stay, down) == up:
+                    analysis_result.append(2)
+                if max(up, stay, down) == stay:
+                    analysis_result.append(1)
+                if max(up, stay, down) == down:
+                    analysis_result.append(0)
+                    
+#--------------------------------------------------------------------- 
+# Handling Map Output
+        # Taken directly from Local Moran's I: moranlocal.py with some
+        # changes to fit the Markov module.
         writer = self.getOutputFromName(self.MAP_OUTPUT).getVectorWriter(
             fields, provider.geometryType(), layer.crs() )
         outFeat = QgsFeature()
@@ -166,7 +203,10 @@ class Markov(GeoAlgorithm):
             inGeom = inFeat.geometry()
             outFeat.setGeometry(inGeom)
             attrs = inFeat.attributes()
-            attrs.append(int(quants_ends_difs[i]))
+            # This requires the field to be added above and for the
+            # new field to be empty. This is additonally appending
+            # the analysis result calculate above.
+            attrs.append(int(analysis_result[i]))
             outFeat.setAttributes(attrs)
             writer.addFeature(outFeat)
             i+=1
@@ -177,7 +217,18 @@ class Markov(GeoAlgorithm):
          self.MAP_OUTPUT))
 
 
-        
+        # Classes defined for map output
+        # End Point:
+        #  Decreasing: The region's final quantile value is lower than its
+        #              initial value.
+        #  Stationary: The region's final quintile value is the same as
+        #              its initial value.
+        #  Increasing: The region's final quantile value is greater than
+        #              its initial value.
+        # Majority Moves:
+        #  Decreasing: The region has a majority of downward moves.
+        #  Stationary: The region remains stationary for a majority of "moves"
+        #  Increasing: The region has a majority of upward moves.
         classes = [0,1,2]
         labels = ["Decreasing", "Stationary", "Increasing"]
         #          Brown         White         Blue
@@ -196,6 +247,7 @@ class Markov(GeoAlgorithm):
 
         # 'expression' is the name of the field that will used
         # to generate the map.
+        # Line 70 above.
         expression = "MARKOV_RES"
         renderer = QgsCategorizedSymbolRendererV2(expression,
                                                   categories)
@@ -204,3 +256,6 @@ class Markov(GeoAlgorithm):
         iface.mapCanvas().refresh()
         iface.legendInterface().refreshLayerSymbology(out_layer)
         iface.mapCanvas().refresh()
+
+
+
